2bdddb3c4d33484a896a1183ea99e2fabe16f03b
user <user@DESKTOP-EKQUFAN>
2022-01-13 08:34:14 +0900

    java problem2-2

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Prob01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in  );
-
+		
+		System.out.print("금액 : ");
+		int input = scanner.nextInt();
+		
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
-
-		/* 코드 작성 */
+		
+		for(int i=0;i<MONEYS.length;i++) {
+			int result = input/MONEYS[i];
+			System.out.println(MONEYS[i]+"원 : "+result+"개");
+			input = input-(MONEYS[i]*result);
+		}
 		
 		scanner.close();
  	}
--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -6,16 +6,23 @@ public class Prob02 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		
 		int[] intArray = new int[ 5 ];
 		double sum = 0;
 
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		for(int i=0;i<intArray.length;i++) {
+			int input = scanner.nextInt();
+			intArray[i] = input; 
+		}
 		
 		/* 배열에 저장된 정수 값 더하기 */
+		for(int i=0;i<intArray.length;i++) {
+			sum += (double)intArray[i];
+		}
 		
 		/* 출력 */
-		
+		System.out.println("평균은 "+sum/intArray.length+" 입니다.");
 		/* 자원정리 */
 		scanner.close();
 	}
